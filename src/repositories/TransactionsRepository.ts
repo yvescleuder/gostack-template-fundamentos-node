a09afe251a9d32bace527592aaa1b63e46d5d59b
Yves Clêuder <yves.cl@live.com>
2020-04-21 21:21:32 -0300

    Melhorias no desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -12,52 +12,41 @@ interface TransactionDTO {
   type: 'income' | 'outcome';
 }
 
-interface Response {
-  transactions: Transaction[];
-  balance: Balance;
-}
-
 class TransactionsRepository {
   private transactions: Transaction[];
 
-  private balance: Balance;
-
   constructor() {
     this.transactions = [];
-    this.balance = {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    };
   }
 
-  public all(): Response {
-    const transactionsComplete = {
-      transactions: this.transactions,
-      balance: this.getBalance(),
-    };
-
-    return transactionsComplete;
+  public all(): Transaction[] {
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    const income = this.transactions.reduce((accumulator, current) => {
-      if (current.type === 'income') return accumulator + current.value;
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        if (transaction.type === 'income')
+          accumulator.income += transaction.value;
+        if (transaction.type === 'outcome')
+          accumulator.outcome += transaction.value;
 
-      return accumulator;
-    }, 0);
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
 
-    const outcome = this.transactions.reduce((accumulator, current) => {
-      if (current.type === 'outcome') return accumulator + current.value;
+    const total = income - outcome;
 
-      return accumulator;
-    }, 0);
-
-    this.balance.income = income;
-    this.balance.outcome = outcome;
-    this.balance.total = this.balance.income - this.balance.outcome;
-
-    return this.balance;
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
   public create({ title, value, type }: TransactionDTO): Transaction {
